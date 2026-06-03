bfd3eb74a7150b3df9008c630271001095f8094f
Luis Núñez <panicjoker03@outlook.com>
2026-06-03 17:11:56 -0600

    .

--- main.py
+++ main.py
@@ -6,8 +6,4 @@ app = FastAPI()
 @app.get("/product/{handle}")
 async def get_product(handle: str):
     # Query Shopify here
-    return {
-        "name": "Gojo Figure",
-        "price": "39.99",
-        "available": True
-    }
+    return 'Yes'
